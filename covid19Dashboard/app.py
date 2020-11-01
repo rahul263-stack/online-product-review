ae5f92e4da633c810c89cb38d8364160429153e0
Rahul <rahulpathak263@gmail.com>
2020-11-01 19:08:16 +0530

    Update app.py

--- covid19Dashboard/app.py
+++ covid19Dashboard/app.py
@@ -2,6 +2,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import webbrowser
+from threading import  Timer
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -197,5 +199,13 @@ app.layout = html.Div(style={'textAlign': 'Center'},  children=[
     )
 ])
 
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:8050/')
+
+
+
+
 if __name__ == '__main__':
+    Timer(1, open_browser).start();
     app.run_server(debug=True)
